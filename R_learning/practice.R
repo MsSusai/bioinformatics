608d50ba24fa3d99106988823439a5d5d1e48709
MsSusai <liuhaorantony@163.com>
2022-04-25 17:00:54 +0800

    commit

---
+++ R_learning/practice.R
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# 作者：Sosai
+# 时间：2022/4/25  16:03 
+# 名称：practice.R
+# 工具：PyCharm
+# 第一题
+# array1 <- array(1:10)
+# print(c(array1[5], array1[3 : 5], array1[c(1, 3, 5, 7)]))
+
+# 第二题
+# data1 <- rnorm(10, 4, 4)
+# print(sort(c(mean(data1), max(data1), min(data1))))
+# print(sort(c(mean(data1), max(data1), min(data1)), decreasing = TRUE))
+
+# 第三题
+# scan1 <- scan()
+# matrix1 <- matrix(scan1, 4, 5)
+# print(c(matrix1[, 1], matrix1[, 3], matrix1[, 5]))
+
+# 第四题
+# rabbit <- data.frame(
+#   sleep_time = c(6.2, 6.1, 6.0, 6.3, 6.1, 5.9, 6.3, 6.5, 6.7, 6.6, 7.1, 6.4,
+#                  6.8, 7.1, 6.6, 6.8, 6.9, 6.6, 5.4, 6.4, 6.2, 6.3, 6.0, 5.9),
+#   drag = gl(4, 6, 24, labels = c("A1", "A2", "A3", "A4")), # 四组，一组六个
+#   food = gl(2, 3, 24, labels = c("B1", "B2"))) # 两组，一组三个
+# rabbit_aov <- aov(sleep_time ~ drag + food, data = rabbit)
+# summary(rabbit_aov)
+
+# 第五题
+
+
