1d6ffc9dd7b2bbe241d27f527fb5f154ee7fc6da
MsSusai <liuhaorantony@163.com>
2022-06-03 16:51:39 +0800

    commit

--- bioinformatics_learning/氨基酸.py
+++ bioinformatics_learning/氨基酸.py
@@ -30,17 +30,76 @@ class Animo:
 		                  'Q', 'M', 'S', 'T',
 		                  'C', 'P', 'H', 'R']
 	
-	def one_to_three(self):
-		triple = []
+	def is_animo(self) -> bool:
+		flag = True
+		if self.seq.isupper() is False:
+			split = re.findall(r'.{3}', self.seq)  # 分割为三个三个一组
+			for i in split:
+				if i.lower().title() in self.animo_three:
+					continue
+				else:
+					flag = False
+					break
+		else:
+			if self.seq.isupper() is True:
+				for i in self.seq:
+					if i in self.animo_one:
+						continue
+					else:
+						flag = False
+						break
+		return flag
 	
-	def three_to_one(self):
-		split = re.findall(r'.{3}', self.seq)  # 分割为三个三个一组
-		single = [self.animo_one[self.animo_three.index(i)] for i in split]
-		return single
+	def one_to_three(self) -> str:
+		try:
+			triple = [self.animo_three[self.animo_one.index(i)] for i in self.seq]
+		except Exception:
+			raise "请检查氨基酸输入是否正确！格式为：‘LWTLH’"
+		return ''.join(triple)
+	
+	def three_to_one(self) -> str:
+		try:
+			split = re.findall(r'.{3}', self.seq)  # 分割为三个三个一组
+			single = [self.animo_one[self.animo_three.index(i)] for i in split]
+		except Exception:
+			raise "请检查氨基酸输入是否正确！格式为：‘LeuTrpThrLeuHis’"
+		return ''.join(single)
+	
+	def percent(self) -> str:
+		alk: int = 0
+		acid: int = 0
+		if self.is_animo():
+			if self.seq.isupper() is False:  # 转换三个字母为一个字母
+				self.seq = self.three_to_one()
+			for animo in self.seq:
+				if animo == 'R' or animo == 'K' or animo == 'H':
+					alk += 1
+				elif animo == 'D' or animo == 'E':
+					acid += 1
+			return f"酸性氨基酸占比{acid / len(self.seq)}，碱性氨基酸占比{alk / len(self.seq)}"
+		else:
+			raise "请检查氨基酸输入是否正确！"
+	
+	def calculate(self) -> str:
+		if self.is_animo():
+			if self.seq.isupper() is False:  # 转换三个字母为一个字母
+				self.seq = self.three_to_one()
+			value: int = len(self.seq) * 128 - (18 * (len(self.seq) - 1))
+			return f"多肽有{len(self.seq)}个氨基酸组成，相对分子量为{value}"
+		else:
+			raise "请检查氨基酸输入是否正确！"
 
 
 if __name__ == '__main__':
-	animo1 = Animo('LeuTrpThrLeuHisPro')
-# print(animo1.three_to_one())
-# animo2 = Animo('LWTLHP')
-# print(animo2.one_to_three())
+	animo1 = Animo('AspLeuTrpThrGluArgArgLeuHisPro')
+	print(animo1.three_to_one())
+	print(animo1.percent())
+	print(animo1.calculate())
+	# animo2 = Animo('RKDEEERRKKDDLWTLHP')
+	# print(animo2.one_to_three())
+	# print(animo2.percent())
+	# print(animo2.calculate())
+	# animo3 = Animo('LWTLHpda')
+	# print(animo3.calculate())
+	# animo4 = Animo('LeuTRPThrLeHis')
+	# print(animo4.calculate())
